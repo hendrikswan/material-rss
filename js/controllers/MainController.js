4ab1e1f8c92c7c85d129d7df97907ec1f83d494b
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2015-01-25 15:29:26 +0200

    added a bunch of lists

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,28 @@
 angular.module( 'MaterialRss', [ 'ngMaterial' ] )
-    .controller("MainController", function(){
-
+    .controller("MainController", function($scope){
+                $scope.budget =
+                {
+                    income:
+                    [
+                        {
+                            description: "salary",
+                            amount: 4500
+                        },
+                        {
+                            description: "tagtree",
+                            amount: 500
+                        }
+                    ],
+                    expenses:
+                    [
+                        {
+                            description: "online services",
+                            amount: 300
+                        },
+                        {
+                            description: "microphone",
+                            amount: 100
+                        }
+                    ]
+                }
     });
