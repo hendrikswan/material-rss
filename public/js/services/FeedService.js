d73bcb4936e67f314381d7e6385497396eba2e38
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2015-01-27 08:52:40 +0200

    showing domain list on grouped items

--- public/js/services/FeedService.js
+++ public/js/services/FeedService.js
@@ -27,67 +27,60 @@ angular.module( 'MaterialRss')
                     f.items.forEach(function(item){
                         item.label = f.label;
                         service.items.push(item);
-                        item.domain = item.link.replace(/(?:(http|htts):\/\/(www.)*)/, '').replace(/\/.*/, '').replace(/\..*/, '');
+                        item.domain = f.meta.title; //item.link.replace(/(?:(http|htts):\/\/(www.)*)/, '').replace(/\/.*/, '').replace(/\..*/, '');
                     })
                 });
 
 
-                function groupByTitle(memo, item){
-                    var matchingGroups = memo.filter(function(lg){
-                        return lg.label == item.label;
-                    });
+                function group(dateFilter){
+                    var groupedByLabel =  _(service.items)
+                        .filter(dateFilter)
+                        .groupBy('label')
+                        .value();
 
-                    if(matchingGroups.length == 0){
-                        var labelGroup = {
-                            label: item.label,
-                            items: [],
-                            isRead: true
-                        }
-                        memo.push(labelGroup);
-                    }else{
-                        var labelGroup = matchingGroups[0];
-                    }
+                    var groupedArr =  _(groupedByLabel)
+                        .keys()
+                        .map(function(label){
+                            var topic =  {
+                                label: label,
+                                items: groupedByLabel[label],
+                            };
 
-                    labelGroup.items.push(item);
-                    if(!item.isRead){
-                        labelGroup.isRead = false;
-                    }
+                            topic.domains = _(topic.items)
+                                .pluck('domain')
+                                .uniq()
+                                .value();
 
-                    return memo;
+                            topic.domainLabel = topic.domains.join(', ');
+
+                            return topic;
+                        })
+                        .value();
+
+
+                    return groupedArr;
                 }
-
-
 
 
                 var dayAgo = new Date().add(-1).days();
                 var weekAgo = new Date().add(-7).days();
                 var monthAgo = new Date().add(-30).days();
 
-                this.grouped.today = this.items
-                    .filter(function(i){
-                        return new Date(i.date) >= dayAgo;
-                    })
-                    .reduce(groupByTitle, []);
+                this.grouped.today = group(function(i){
+                    return new Date(i.date) >= dayAgo;
+                });
 
-                this.grouped.thisWeek = this.items
-                    .filter(function(i){
-                        return new Date(i.date) < dayAgo &&  new Date(i.date) >= weekAgo;
-                    })
-                    .reduce(groupByTitle, []);
+                this.grouped.thisWeek = group(function(i){
+                    return new Date(i.date) < dayAgo &&  new Date(i.date) >= weekAgo;
+                });
 
-                this.grouped.thisMonth = this.items
-                    .filter(function(i){
-                        return new Date(i.date) < weekAgo &&  new Date(i.date) >= monthAgo;
-                    })
-                    .reduce(groupByTitle, []);
+                this.grouped.thisMonth = group(function(i){
+                    return new Date(i.date) < weekAgo &&  new Date(i.date) >= monthAgo;
+                });
 
-                this.grouped.old = this.items
-                    .filter(function(i){
+                this.grouped.old = group(function(i){
                         return new Date(i.date) < monthAgo;
-                    })
-                    .reduce(groupByTitle, []);
-
-                    console.log(this.grouped.thisWeek);
+                });
 
 
 
